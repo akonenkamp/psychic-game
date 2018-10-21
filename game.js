4a86e84d0f72e232fde57a5b4be4b302e0ebe812
Konenkamp <n0267144@lm.lmig.com>
2018-10-20 17:14:23 -0700

    game checks user number for the guessed number and if correct breaks out of loop

--- game.js
+++ game.js
@@ -1,22 +1,28 @@
 
 
-var userGuess = 100; //highest number user can guess 
+var userGuess = 5; //highest number user can guess 
 var counter = 0;
-var maxTries = 3;
+var maxTries = 5;
 
 // generate random number and store it for later 
 
-var randomNumber = Math.floor(Math.random() * userGuess) + 1;
-//prompt user to guess number between 1 and X 
+randomNumber = Math.floor((Math.random() * userGuess) + 1);
 
+
+//prompt user to guess number between 1 and X 
 while (userAttempts !== randomNumber) {
 	var userAttempts = prompt("pick a number between one and " + userGuess);
 	counter += 1 ;
 
 	if (counter > maxTries) {
-		alert("you have no more tries left;");
+		document.write("you have no more tries left;");
 
 		break;
 	} 
+
+	if (userAttempts == randomNumber ) {
+		document.write("you win, the correct number was " + randomNumber);
+		break;
+	}
 
 }
