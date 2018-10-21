4f143c7dfeaf9871c897350260b8398d175c7215
Konenkamp <n0267144@lm.lmig.com>
2018-10-20 18:01:57 -0700

    cleaned up messaging to user and increased difficulty

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 
 
-var userGuess = 5; //highest number user can guess 
+var userGuess = 50; //highest number user can guess 
 var counter = 0;
-var maxTries = 5;
+var maxTries = 10;
 
 // generate random number and store it for later 
 
@@ -12,15 +12,14 @@ randomNumber = Math.floor((Math.random() * userGuess) + 1);
 //prompt user to guess number between 1 and X 
 while (userAttempts !== randomNumber) {
 	var userAttempts = prompt("pick a number between one and " + userGuess);
-	counter += 1 ;
+	counter += 1;
 
 	if (counter > maxTries) {
-		document.write("you have no more tries left;");
-
+		document.write("you have no more tries left, the correct number was " + randomNumber);
 		break;
-	} 
+	}
 
-	if (userAttempts == randomNumber ) {
+	if (userAttempts == randomNumber) {
 		document.write("you win, the correct number was " + randomNumber);
 		break;
 	}
